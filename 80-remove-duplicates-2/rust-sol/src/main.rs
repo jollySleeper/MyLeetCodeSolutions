58d0c4f1187383dd3ba6c9bbab6f3b6bd732ef2a
jollySleeper <git@pm21.anonaddy.com>
2023-09-25 08:31:01 +0530

    80-Remove-Duplicates-II: Added: Optimized Solution

--- 80-remove-duplicates-2/rust-sol/src/main.rs
+++ 80-remove-duplicates-2/rust-sol/src/main.rs
@@ -5,7 +5,7 @@
 
 #[allow(dead_code)]
 
-fn remove_duplicate_n(nums: &mut Vec<i32>) -> i32 {
+fn remove_duplicate_n_wrong(nums: &mut Vec<i32>) -> i32 {
     if nums.len() <= 2 {
         return nums.len().try_into().unwrap();
     }
@@ -65,14 +65,31 @@ fn remove_duplicate_n(nums: &mut Vec<i32>) -> i32 {
 // 0, 0, 1, 1, 2, 1, 2, 3, 3 i2 j4 max_index_traveresed = 6
 // 0, 0, 1, 1, 2, 3, 2, 3, 3 i3 j5 max_index_traveresed = 7
 // 0, 0, 1, 1, 2, 3, 3, 3, 3 i4 j6 max_index_traveresed = 8
-//
+
+fn remove_duplicate_n(nums: &mut Vec<i32>) -> i32 {
+    if nums.len() <= 2 {
+        return nums.len().try_into().unwrap();
+    }
+
+    let mut i = 2;
+    let mut j = 2;
+    while j < nums.len() {
+        if nums[j] != nums[j - 2] {
+            nums[i] = nums[j];
+            i += 1;
+        }
+        j += 1;
+    }
+
+    return (i).try_into().unwrap();
+}
 
 fn main() {
     println!("Hello, world!");
-    // let mut nums: Vec<i32> = Vec::from([1, 1, 1, 2, 2, 3]);
+    let mut nums: Vec<i32> = Vec::from([1, 1, 1, 2, 2, 3]);
     // let mut nums: Vec<i32> = Vec::from([0, 0, 1, 1, 1, 1, 2, 3, 3]);
     // let mut nums: Vec<i32> = Vec::from([0, 0, 1, 1, 1, 1, 2, 3, 3, 4, 4, 4]);
-    let mut nums: Vec<i32> = Vec::from([1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3]);
+    // let mut nums: Vec<i32> = Vec::from([1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3]);
 
     println!("Result {:?}", remove_duplicate_n(&mut nums));
 }
