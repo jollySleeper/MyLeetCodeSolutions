0be02e9e64c8f7c567a98fc5dcbe0f0c18e60a0d
jollySleeper <git@pm21.anonaddy.com>
2024-05-25 16:05:44 +0530

    Solved: 152-Max-Product-Subarray: Rust: O(n) Solution

--- 152-maximum-product-subarray/rust/src/main.rs
+++ 152-maximum-product-subarray/rust/src/main.rs
@@ -1,29 +1,24 @@
+use std::cmp::max;
+
 pub fn max_product(nums: Vec<i32>) -> i32 {
     if nums.len() == 1 {
         return nums[0];
     }
 
     let mut final_product = i32::MIN;
-    let mut product_sig = 1;
-    let mut product_usig = 1;
+    let mut product_prefix = 1;
+    let mut product_suffix = 1;
 
     for index in 0..nums.len() {
-        product_sig *= nums[index];
-        product_usig *= nums[index];
+        product_prefix *= nums[index];
+        product_suffix *= nums[nums.len() - 1 - index];
 
-        let mut tmp = 0;
-        if product_usig >= product_sig {
-            tmp = product_usig;
+        final_product = max(final_product, max(product_prefix, product_suffix));
+        if product_prefix == 0 {
+            product_prefix = 1;
         }
-        if product_usig < product_sig {
-            tmp = product_sig;
-        }
-        if final_product < tmp {
-            final_product = tmp;
-        }
-
-        if product_usig <= 0 {
-            product_usig = 1
+        if product_suffix == 0 {
+            product_suffix = 1;
         }
     }
 
@@ -33,9 +28,9 @@ pub fn max_product(nums: Vec<i32>) -> i32 {
 fn main() {
     println!("Hello, world!");
 
-    let nums = vec![2, 3, -2, 4];
-    let nums2 = Vec::from([-3, -1, -1]);
-    let nums3 = Vec::from([3, -1, 4]);
+    let _nums = vec![2, 3, -2, 4];
+    let _nums2 = Vec::from([-3, -1, -1]);
+    let _nums3 = Vec::from([3, -1, 4]);
     let nums4 = Vec::from([2, -5, -2, -4, 3]);
 
     println!("Answer {:?}", max_product(nums4));
