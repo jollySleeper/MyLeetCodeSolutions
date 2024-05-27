1da9086594aacf232a2a0bcde969503748ac3dc8
jollySleeper <git@pm21.anonaddy.com>
2024-05-27 08:47:54 +0530

    Solved: 46-Permutations: Go: O(n!*n) Solution Using Swaps

--- 46-permutations/go/main.go
+++ 46-permutations/go/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
+
+func permute(nums []int) [][]int {
+	var ans [][]int
+	recursive_permute(0, nums, ans)
+
+	return ans
+}
+
+func swap(i int, j int, nums []int) {
+	temp := nums[i]
+	nums[i] = nums[j]
+	nums[j] = temp
+}
+
+func recursive_permute(index int, nums []int, ans [][]int) {
+	fmt.Println("Ans = ", ans)
+	if index >= len(nums) {
+		fmt.Println("Permutation = ", nums)
+
+		permutation := slices.Clone(nums)
+		// Pass By Reference Occuring Here
+		// Don't Know How to Pass 'nums' By Value
+		ans = append(ans, permutation)
+		fmt.Println("Ans = ", ans)
+
+		return
+	}
+
+	for i := index; i < len(nums); i++ {
+		swap(index, i, nums)
+		fmt.Println("Ans = ", ans)
+		recursive_permute(index+1, nums, ans)
+		swap(i, index, nums)
+	}
+}
 
 func main() {
-	fmt.Println("Hello, world! ~ GO")
+	nums := []int{1, 2, 3}
+	fmt.Println("Nums are = ", nums)
+	fmt.Println("Permutations are = ", permute(nums))
 }
