824b2572e5737764d7fdec753b7f7dcc8ef0d9f1
jollySleeper <git@pm21.anonaddy.com>
2024-05-27 11:06:39 +0530

    Solved: 217-Contains-Duplicate: Js: O(n^2) Solution: TLE

--- 217-contains-duplicate/js/main.js
+++ 217-contains-duplicate/js/main.js
@@ -1,1 +1,19 @@
-console.log('Hello, world! ~ JS')
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j]) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+};
+
+let nums = [1,2,3,1];
+console.log(nums);
+console.log("Contains Duplicate = " + containsDuplicate(nums));
