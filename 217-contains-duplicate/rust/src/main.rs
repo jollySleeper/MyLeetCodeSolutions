13b229af54f272f1cb10be397a86fe49d4269d44
jollySleeper <git@pm21.anonaddy.com>
2024-05-27 11:18:58 +0530

    Solved: 217-Contains-Duplicate: Rust: O(n) Solution

--- 217-contains-duplicate/rust/src/main.rs
+++ 217-contains-duplicate/rust/src/main.rs
@@ -1,3 +1,21 @@
+use std::collections::HashMap;
+
+fn contains_duplicate(nums: Vec<i32>) -> bool {
+    let mut map: HashMap<i32, bool> = HashMap::new();
+    map.insert(nums[0], true);
+
+    for index in 1..nums.len() {
+        if map.contains_key(&nums[index]) == true {
+            return true;
+        }
+        map.insert(nums[index], true);
+    }
+
+    return false;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let nums = Vec::from([1, 2, 3, 1]);
+    println!("Nums = {:?}", nums);
+    println!("Contains Duplicate = {}", contains_duplicate(nums));
 }
