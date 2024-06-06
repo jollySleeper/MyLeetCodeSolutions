72088eb37de598445f8d54718c682c236e1ef4cf
jollySleeper <git@pm21.anonaddy.com>
2024-06-06 09:26:30 +0530

    Solved: 28-StrStr-Function: C++: Optimize O(n*m) Soln

--- 28-strstr-function/cpp/main.cpp
+++ 28-strstr-function/cpp/main.cpp
@@ -3,19 +3,23 @@
 using namespace std;
 
 int strStr(string haystack, string needle) {
+    if (needle.length() < 1 ) {
+        return 0;
+    }
+    if (haystack.length() < 1 ) {
+        return -1;
+    }
     if (needle.length() > haystack.length()) {
         return -1;
     }
 
-    int window = needle.length() - 1;
-    for (int i = 0; i < haystack.length(); i++) {
-        for (int j = i, k = 0; k < needle.length(); j++, k++) {
-            if (needle[k] == haystack[j]) {
-                if (k == needle.length() - 1) {
-                    return i;
-                }
-            } else {
+    for (int i = 0; i < haystack.length() - needle.length(); i++) {
+        for (int j = 0; j < needle.length(); j++) {
+            if (needle[j] != haystack[i + j]) {
                 break;
+            }
+            if (j == needle.length() - 1) {
+                return i;
             }
         }
     }
@@ -24,8 +28,8 @@ int strStr(string haystack, string needle) {
 }
 
 int main() {
-    string haystack = "leetcode";
-    string needle = "leeto";
+    string haystack = "codeleetcode";
+    string needle = "leet";
     cout << "Found at Index = " << strStr(haystack, needle);
 
     return 0;
