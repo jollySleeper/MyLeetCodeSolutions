0c17912d3717c141f5e0b8ee7b1d017de733e370
jollySleeper <git@pm21.anonaddy.com>
2024-06-06 09:14:26 +0530

    Solved: 28-StrStr-Function: C++: O(n*m) Soln

--- 28-strstr-function/cpp/main.cpp
+++ 28-strstr-function/cpp/main.cpp
@@ -2,7 +2,31 @@
 
 using namespace std;
 
+int strStr(string haystack, string needle) {
+    if (needle.length() > haystack.length()) {
+        return -1;
+    }
+
+    int window = needle.length() - 1;
+    for (int i = 0; i < haystack.length(); i++) {
+        for (int j = i, k = 0; k < needle.length(); j++, k++) {
+            if (needle[k] == haystack[j]) {
+                if (k == needle.length() - 1) {
+                    return i;
+                }
+            } else {
+                break;
+            }
+        }
+    }
+
+    return -1;
+}
+
 int main() {
-    cout << "Hello, world! ~ C++" << endl;
+    string haystack = "leetcode";
+    string needle = "leeto";
+    cout << "Found at Index = " << strStr(haystack, needle);
+
     return 0;
 }
