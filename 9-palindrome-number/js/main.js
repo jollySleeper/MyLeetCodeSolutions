34f713b3f7f9630505afbc2a66166e17cff98dd8
jollySleeper <git@pm21.anonaddy.com>
2024-06-06 09:46:35 +0530

    Solved: 9-Palindrome-Number: JS: O(n) Soln

--- 9-palindrome-number/js/main.js
+++ 9-palindrome-number/js/main.js
@@ -1,1 +1,30 @@
-console.log('Hello, world! ~ JS')
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if (x < 0) {
+        return false;
+    }
+    if (x == 0) {
+        return  true;
+    }
+
+    let nums = [];
+    while (x > 0) {
+        nums.push(x % 10);
+        x = x/10;
+        x = Math.floor(x);
+    }
+
+    for (let index = 0; index < (nums.length / 2); index++) {
+        if (nums[index] != nums[nums.length - 1 - index]) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+let num = 12011;
+console.log(`Number '${num}' is Palindrome = ${isPalindrome(num)}`);
