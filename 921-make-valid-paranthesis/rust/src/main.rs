f74f7ec31ac99596ea211282bc9ef2f976c8b4b9
jollySleeper <git@pm21.anonaddy.com>
2024-06-07 12:22:26 +0530

    Solved: 921-Make-Valid-Paranthesis: Rust: O(n) Soln

--- 921-make-valid-paranthesis/rust/src/main.rs
+++ 921-make-valid-paranthesis/rust/src/main.rs
@@ -1,3 +1,21 @@
+fn min_add_to_make_valid(s: String) -> i32 {
+    let mut stack: Vec<char> = Vec::new();
+    for ch in s.chars() {
+        if stack.len() > 0 && stack[stack.len() - 1] == '(' && ch == ')' {
+            stack.pop();
+            continue;
+        }
+        stack.push(ch);
+    }
+
+    return stack.len() as i32;
+}
+
 fn main() {
     println!("Hello, world!");
+    let input = "((()))".to_string();
+    println!(
+        "Opertaions Reqd to Make Paranthesis Valid is {}",
+        min_add_to_make_valid(input)
+    );
 }
