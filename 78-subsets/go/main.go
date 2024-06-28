456ab1376dd756c661cf62e105bf5a5866bcbf98
jollySleeper <git@pm21.anonaddy.com>
2024-06-28 18:44:27 +0530

    Solved: 78-Subsets: Go: O(2n) Solution

--- 78-subsets/go/main.go
+++ 78-subsets/go/main.go
@@ -2,6 +2,28 @@ package main
 
 import "fmt"
 
-func main () {
-fmt.Println("Hello, world! ~ GO")
+func subsets(nums []int) [][]int {
+    var allSubsets [][]int 
+    subset :=  []int {}
+    getSubset(nums, subset, &allSubsets, 0)
+
+    return allSubsets
+}
+
+func getSubset(nums []int, subset []int, allSubsets *[][]int, index int)  {
+    tempSubset := make([]int, len(subset))
+    copy(tempSubset, subset)
+    *allSubsets = append(*allSubsets, tempSubset)
+
+    for i := index; i < len(nums); i++ {
+        subset = append(subset, nums[i])
+        getSubset(nums, subset, allSubsets, i + 1)
+        subset = subset[:len(subset)-1]
+    }
+}
+
+func main() {
+    nums := []int {1,2,3}
+    fmt.Println("Nums are = ", nums)
+	fmt.Println("Subsets are = ", subsets(nums))
 }
