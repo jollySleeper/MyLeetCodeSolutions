e193e0d3b4b4d43e3bedf2950995c04cd97800b1
jollySleeper <git@pm21.anonaddy.com>
2024-06-29 15:23:11 +0530

    Solved: 48-Rotate-Image: Rust: O(n*m) Solution

--- 48-rotate-image/README.md
+++ 48-rotate-image/README.md
@@ -1,1 +1,7 @@
 # 48-rotate-image
+
+## Transpose of Matrix & Reverse The Rows
+- Diagonal Remains the Same
+- Only Traverse the Upper Triange OR Lower Triangle to Swap elements
+- Swap Operation will swap elements of Upper Triangle to Lower Triangle
+- Only Need to Traver Upper Triangle i.e j > i
--- 48-rotate-image/rust/src/main.rs
+++ 48-rotate-image/rust/src/main.rs
@@ -1,5 +1,33 @@
 pub struct Solution;
 
+impl Solution {
+    pub fn rotate(matrix: &mut Vec<Vec<i32>>) {
+        // Transponse Matrix: Row to Column, Column to Row
+        let row = matrix.len();
+        let column = matrix[0].len();
+        for i in 0..row {
+            for j in (i + 1)..column {
+                let tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+
+        // Reversing the Rows of Transposed Matrix
+        let col = matrix[0].len();
+        for i in 0..matrix.len() {
+            for j in 0..(col / 2) {
+                let tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][col - 1 - j];
+                matrix[i][col - 1 - j] = tmp;
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world! ~ Rust");
+    let mut nums: Vec<Vec<i32>> = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+    println!("Rotation of Nums {:?}", nums);
+    Solution::rotate(&mut nums);
+    println!("Rotated Nums {:?}", nums);
 }
